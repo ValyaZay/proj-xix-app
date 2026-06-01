7974b0cd7d9f0b11cfdd0c6e28c890a832061550
Valya Zaitseva <val.zaitseva@gmail.com>
2026-06-01 12:50:06 +0000

    add init bank ix

--- anchor/programs/bank/src/lib.rs
+++ anchor/programs/bank/src/lib.rs
@@ -24,24 +24,32 @@ pub use shares_math::*;
 pub mod transfer_helpers;
 pub use transfer_helpers::*;
 
-declare_id!("BaJR9Z98XwiSg3zNBCwm6XTYF2WW3XiePnd42aNkQpLy");
+declare_id!("5A3GtQVu573XH7XyqUdg2fwn3P4hCorg5oC61JZMUjUQ");
 
 #[program]
 pub mod bank {
     use super::*;
 
-    // pub fn init_bank(ctx: Context<InitBank>, mint_address: Pubkey) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn init_bank(ctx: Context<InitBank>) -> Result<()> {
+        let bank = &mut ctx.accounts.bank_state;
+        bank.set_inner(Bank { 
+            authority: ctx.accounts.authority.key(),
+            mint: ctx.accounts.mint.key(),
+            total_deposits: 0, 
+            total_deposit_shares: 0,
+            is_initialized: true });
+        // init bank token account 
+        Ok(())
+    }
 
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
-        require!(amount > 0, BankError::ZeroAmountToDeposit);
+        require!(amount > 0, BankErrors::ZeroAmountToDeposit);
 
         let bank_state = &mut ctx.accounts.bank_state;
-        require!(bank_state.is_initialized, BankError::BankIsNotInitialized);
+        require!(bank_state.is_initialized, BankErrors::BankIsNotInitialized);
 
         // invariant check
-        require!(ctx.accounts.bank_token_account.amount >= bank_state.total_deposits, BankError::BankUnderfunded);
+        require!(ctx.accounts.bank_token_account.amount >= bank_state.total_deposits, BankErrors::BankUnderfunded);
 
         let user_state = &mut ctx.accounts.user_state;
         if !user_state.is_initialized {
@@ -72,12 +80,12 @@ pub mod bank {
             convert_assets_to_shares(amount, bank_state.total_deposit_shares, bank_state.total_deposits)
         };
 
-        require!(shares > 0, BankError::ZeroSharesFromAmount);
+        require!(shares > 0, BankErrors::ZeroSharesFromAmount);
 
         // update bank and user state
-        bank_state.total_deposit_shares = bank_state.total_deposit_shares.checked_add(shares).ok_or(MathError::Overflow)?;
-        bank_state.total_deposits = bank_state.total_deposits.checked_add(amount).ok_or(MathError::Overflow)?;
-        user_state.deposit_usdc_shares = user_state.deposit_usdc_shares.checked_add(shares).ok_or(MathError::Overflow)?;
+        bank_state.total_deposit_shares = bank_state.total_deposit_shares.checked_add(shares).ok_or(BankErrors::Overflow)?;
+        bank_state.total_deposits = bank_state.total_deposits.checked_add(amount).ok_or(BankErrors::Overflow)?;
+        user_state.deposit_usdc_shares = user_state.deposit_usdc_shares.checked_add(shares).ok_or(BankErrors::Overflow)?;
 
         // emit event
         emit!(DepositEvent {
@@ -89,6 +97,36 @@ pub mod bank {
 
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct InitBank<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = Bank::DISCRIMINATOR.len() + Bank::INIT_SPACE,
+        seeds = [SEED_BANK_STATE, mint.key().as_ref()],
+        bump
+    )]
+    pub bank_state: Account<'info, Bank>,
+
+    #[account(
+        init,
+        payer = authority, 
+        token::mint = mint,
+        token::authority = bank_token_account,
+        seeds = [SEED_BANK_TOKEN_ACCOUNT, mint.key().as_ref()],
+        bump
+    )]
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -107,8 +145,8 @@ pub struct Deposit<'info> {
 
     #[account(
         mut,
-        has_one = mint @ BankError::MintForBankIsWrong,
-        constraint = bank_state.mint.key() == user_associated_token_account.mint.key() @ BankError::UserAtaForBankIsWrong,
+        has_one = mint @ BankErrors::MintForBankIsWrong,
+        constraint = bank_state.mint.key() == user_associated_token_account.mint.key() @ BankErrors::UserAtaForBankIsWrong,
         seeds = [SEED_BANK_STATE, mint.key().as_ref()],
         bump,
     )]
@@ -127,8 +165,8 @@ pub struct Deposit<'info> {
     #[account(
         mut,
         seeds = [SEED_BANK_TOKEN_ACCOUNT, mint.key().as_ref()],
-        constraint = bank_token_account.mint == mint.key() @ BankError::MintForBankIsWrong,
-        constraint = bank_token_account.owner == bank_token_account.key() @ BankError::BankTokenAccountOwnerIsWrong,
+        constraint = bank_token_account.mint == mint.key() @ BankErrors::MintForBankIsWrong,
+        constraint = bank_token_account.owner == bank_token_account.key() @ BankErrors::BankTokenAccountOwnerIsWrong,
         bump,
     )]
     pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
