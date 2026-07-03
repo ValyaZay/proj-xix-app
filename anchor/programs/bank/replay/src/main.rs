b900f8b19cc533811eece7820655161f01c75c33
Valya Zaitseva <val.zaitseva@gmail.com>
2026-07-03 10:07:20 +0000

    state replay for one and all users

--- anchor/programs/bank/replay/src/main.rs
+++ anchor/programs/bank/replay/src/main.rs
@@ -6,16 +6,14 @@ use bank::constants::*;
 use bank::events::{BankSnapshot, DepositEvent, EventJsonModel, EventType};
 
 fn main() {
-    // try using a constant from a bank    
-    //println!("MAX_USDC_DEPOSIT = {}", MAX_USDC_DEPOSIT);
     let mut bank = ReplayBank {
         total_deposits: 0,
         total_deposit_shares: 0
     };
-    state_replay_events_from_source(&mut bank);
+    state_replay_events_from_source(&mut bank, false, "7HtyBjjyoDhPC2ihjPrMPUNXz6JuT6Rb2nQ8AxF9cHD6");
 }
 
-fn state_replay_events_from_source(bank: &mut ReplayBank) {
+fn state_replay_events_from_source(bank: &mut ReplayBank, replay_for_one_user: bool, user: &str) {
     //println!("Replay events for user {}...", user); //add a user to params
     let file = File::open("programs/bank/bank_events_stream.jsonl").unwrap();
     let reader = BufReader::new(file);
@@ -23,38 +21,48 @@ fn state_replay_events_from_source(bank: &mut ReplayBank) {
         let line = line.unwrap();
         let event_json_model: EventJsonModel = serde_json::from_str(&line).unwrap();
         
-        
-        // here replay for one user or all and print intermediate result
-        match event_json_model.event_type {
-            EventType::Deposit => {
-                println!("Replaying step {}...", event_json_model.step);
-                let event = DepositEvent::try_from_slice(&event_json_model.data).unwrap();
-                println!("Deposit {}", event.amount);
-                
-                bank.total_deposits = bank.total_deposits + event.amount;
-                bank.total_deposit_shares = bank.total_deposit_shares + event.shares;
-                println!("{:?}", bank);
-                println!();
-            },
-            EventType::Withdraw => {
-                println!("Replaying step {}...", event_json_model.step);
-                let event = DepositEvent::try_from_slice(&event_json_model.data).unwrap();
-                println!("Withdraw {}", event.amount);
-               
-                bank.total_deposits = bank.total_deposits - event.amount;
-                bank.total_deposit_shares = bank.total_deposit_shares - event.shares; 
-                println!("{:?}", bank);
-                println!();
-            },
-            EventType::BankSnapshot => {
-                println!("Final assert");
-                let event = BankSnapshot::try_from_slice(&event_json_model.data).unwrap();
-                println!("{:?}", event);
-                println!("{:?}", bank);
-                assert!(bank.total_deposits == event.total_deposits);
-                assert!(bank.total_deposit_shares == event.total_deposit_shares);
-            },
+        if replay_for_one_user == true {
+            if event_json_model.user.as_str() == user {
+                perform_match(bank, event_json_model);
+            }
+        } else {
+            perform_match(bank, event_json_model);
         }
+    }
+}
+
+fn perform_match(bank: &mut ReplayBank, event_json_model: EventJsonModel) {
+    // here replay for one user or all and print intermediate result
+    match event_json_model.event_type {
+        EventType::Deposit => {
+            println!("Replaying step {}, user {} ...", event_json_model.step, event_json_model.user);
+            let event = DepositEvent::try_from_slice(&event_json_model.data).unwrap();
+            println!("Deposit {}", event.amount);
+            
+            bank.total_deposits = bank.total_deposits + event.amount;
+            bank.total_deposit_shares = bank.total_deposit_shares + event.shares;
+            println!("{:?}", bank);
+            println!();
+        },
+        EventType::Withdraw => {
+            println!("Replaying step {}, user {} ...", event_json_model.step, event_json_model.user);
+            let event = DepositEvent::try_from_slice(&event_json_model.data).unwrap();
+            println!("Withdraw {}", event.amount);
+            
+            bank.total_deposits = bank.total_deposits - event.amount;
+            bank.total_deposit_shares = bank.total_deposit_shares - event.shares; 
+            println!("{:?}", bank);
+            println!();
+        },
+        EventType::BankSnapshot => {
+            println!("Snapshot, user {} ...", event_json_model.user);
+            let event = BankSnapshot::try_from_slice(&event_json_model.data).unwrap();
+            println!("{:?}", event);
+            println!("{:?}", bank);
+            assert!(bank.total_deposits == event.total_deposits);
+            assert!(bank.total_deposit_shares == event.total_deposit_shares);
+            println!();
+        },
     }
 }
 
