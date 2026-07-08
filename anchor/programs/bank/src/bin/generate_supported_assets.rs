b85bf204b6bb252087a9ec9ee5ded6979b217ceb
Valya Zaitseva <val.zaitseva@gmail.com>
2026-07-08 12:21:57 +0000

    add crate for supported_assets storage

---
+++ anchor/programs/bank/src/bin/generate_supported_assets.rs
@@ -0,0 +1,34 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+use anchor_lang::prelude::*;
+use bank::{ USDC_MAINNET, WRAPPED_BTC_MAINNET };
+use serde::{Deserialize, Serialize};
+
+pub fn main() {
+    set_supported_assets(Pubkey::from_str_const(USDC_MAINNET), String::from("USDC"));
+    set_supported_assets(Pubkey::from_str_const(WRAPPED_BTC_MAINNET), String::from("BTC"));
+}
+
+fn set_supported_assets(mint: Pubkey, symbol: String) {
+    println!("set config: mint {}, symbol {}", mint, symbol);
+    
+    let asset = SupportedAsset {
+        mint: mint,
+        symbol: symbol,
+    };
+    let asset_string = serde_json::to_string(&asset).unwrap();
+
+    let path = format!("programs/bank/src/bin/supported_assets.jsonl");
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path.as_str()).unwrap();
+
+    writeln!(file, "{}", asset_string).unwrap();
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SupportedAsset {
+    pub mint: Pubkey,
+    pub symbol: String,
+}
